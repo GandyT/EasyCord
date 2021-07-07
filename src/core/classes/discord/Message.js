2842a7aa87d1eb014c44607f1a5d2ab15cef7d2d
culinaryAmphibian <teperbudget@gmail.com>
2021-07-06 22:04:06 -0400

    common methods that idk how to implement

--- src/core/classes/discord/Message.js
+++ src/core/classes/discord/Message.js
@@ -2,4 +2,20 @@ class Message {
     constructor() {
 
     }
+    edit(content)
+    {
+
+    }
+    delete(opts = {timeout: 0})
+    {
+
+    }
+    react()
+    {
+
+    }
+    get sentTimeStamp()
+    {
+        
+    }
 }
